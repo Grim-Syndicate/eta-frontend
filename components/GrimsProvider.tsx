1dc12b423a377719d9eb67c44f1cfee409f03038
grimAgent <90475821+grimAgent@users.noreply.github.com>
2022-08-29 14:11:15 -0300

    Cleaned up index a bit

--- components/GrimsProvider.tsx
+++ components/GrimsProvider.tsx
@@ -1,29 +1,55 @@
 import React, { createContext, useEffect, useState } from "react";
 import { useWallet } from '@solana/wallet-adapter-react';
+import { useRouter } from 'next/router'
 import axios from "axios";
+import Utils from "../utils/Utils";
 
 const GrimsContext = createContext<any | undefined>(undefined);
 
 let ONE_COOLDOWN_PERIOD = (24 * 60 * 60 * 1000);
 let WALLET_COOLDOWN_RATE = 1;
+let SOL_PRICE = 0;
+let FLOOR_PRICE = 0;
 const COOLDOWN_UNITS = 10; // used to show a more precise number when displaying the cooldown
 const domainURL = process.env.NEXT_PUBLIC_API_URL || '';
 const getStakedURL = domainURL + "/grims-state";
 const userRolesURL = domainURL + "/userRoles";
+const getPublicStateURL = domainURL + "/public-state";
+
+let loadPublicStateRefresh: NodeJS.Timeout | undefined = undefined
+let isPublicStateLoading = false
 
 function GrimsProvider({ children }:any) {
+  const router = useRouter()
+  const { walletOverride } = router.query
   const { publicKey } = useWallet();
   const [isUsingLedger, setIsUsingLedger] = useState(false);
   const [grims, setGrims] = useState<{[key:string]: any}>({});
   const [daemons, setDaemons] = useState({});
   const [isLoadingGrims, setIsLoadingGrims] = useState(false);
+  const [isGrimsLoaded, setIsGrimsLoaded] = useState(false);
   const [stakedGrims, setStakedGrims] = useState<any[]>([]);
   const [grimsReady, setGrimsReady] = useState<any[]>([]);
   const [grimCount, setGrimCount] = useState<number>();
 
+  const [pointsBalance, setPointsBalance] = useState(0);
+  const [pointsLocked, setPointsLocked] = useState(0);
+  const [pointsUnclaimed, setPointsUnclaimed] = useState(0);
+  const [airdropApproved, setAirdropApproved] = useState(false);
+  const [hasOnlyUnstakedGrimsInWallet, setHasOnlyUnstakedGrimsInWallet] = useState(false);
+  const [isClockingIn, setIsClockingIn] = useState<{[key:string]: boolean}>({});
+  const [isClockingOut, setIsClockingOut] = useState<{[key:string]: boolean}>({});
+  const [notStakedGrims, setNotStakedGrims] = useState<any[]>([]);
+  const [hiddenGrims, setHiddenGrims] = useState<any[]>([]);
+
   const [userRoles, setUserRoles] = useState<Array<string>>([]);
   const canCreateRaffle = userRoles && userRoles.includes("RAFFLE_CREATOR");
   const canCreateProposal = userRoles && userRoles.includes("PROPOSAL_CREATOR");
+
+  const [isPublicStateLoaded, setIsPublicStateLoaded] = useState(false);
+  const [allStakedGrims, setAllStakedGrims] = useState(0);
+  const [lockedValueSOL, setLockedValueSOL] = useState(0);
+  const [lockedValueUSD, setLockedValueUSD] = useState(0);
 
   const loadRoles = async() => {
     if (!publicKey) return;
@@ -55,6 +81,21 @@ function GrimsProvider({ children }:any) {
     data();
   }, [publicKey]);
 
+  useEffect(() => {
+    if(isPublicStateLoading) return
+    async function data() {
+      loadPublicState();
+    } 
+    data();
+
+    return () => {
+      if (loadPublicStateRefresh) clearInterval(loadPublicStateRefresh);
+      setAllStakedGrims(0);
+      setLockedValueSOL(0);
+      setLockedValueUSD(0);
+    }
+  }, []);
+
   function getTimestamp() {
     let now = new Date();
 
@@ -70,18 +111,18 @@ function GrimsProvider({ children }:any) {
 
     if (publicKey) {
       let allGrims:{[key:string]: any} = {};
+      let allDaemons:{[key:string]: any} = {};
       let grimsInWallet = 0
 
       try {
-        let result = await axios.get(getStakedURL + "?wallet=" + publicKey);
+        let result = await axios.get(getStakedURL + "?wallet=" + (walletOverride || publicKey));
         let staked:any[] = [];
         let notStaked = [];
         let hidden = [];
 
         if (result && result.data && result.data.success && result.data.wallet) {
-          //setPointsBalance(formatPoints(result.data.wallet.pointsBalance));
-          //setAirdropApproved(result.data.airdropApproved);
-          //setDepartmentsBreakdown(result.data.departments);
+          setPointsBalance(parseFloat(Utils.formatPoints(result.data.wallet.pointsBalance)));
+          setAirdropApproved(result.data.airdropApproved);
 
           WALLET_COOLDOWN_RATE = parseFloat(result.data.wallet.walletCooldownRate);
           ONE_COOLDOWN_PERIOD = parseFloat(result.data.wallet.oneCooldownInterval);
@@ -125,8 +166,10 @@ function GrimsProvider({ children }:any) {
             grimsInWallet += 1
           }
 
-          //handleInitialTabSelection(stakedResults, unstakedResults)
-
+          const hasStakedGrims = stakedResults && Object.keys(stakedResults).length > 0
+          const hasUnstakedGrims = unstakedResults && Object.keys(unstakedResults).length > 0
+          setHasOnlyUnstakedGrimsInWallet(!hasStakedGrims && hasUnstakedGrims)
+      
           let benchedResults = result.data.wallet.benched;
 
           for (let i in benchedResults) {
@@ -148,18 +191,25 @@ function GrimsProvider({ children }:any) {
             hidden.push(allGrims[i]);
           }
 
+          let daemonsResults = result.data.wallet.daemons;
+
+          for (let i in daemonsResults) {
+            allDaemons[i] = daemonsResults[i];
+          }
+
           setGrims(allGrims);
           setGrimCount(grimsInWallet)
+          setDaemons(allDaemons);
 
-          //setIsClockingIn(clockingIn);
-          //setIsClockingOut(clockingOut);
+          setIsClockingIn(clockingIn);
+          setIsClockingOut(clockingOut);
 
-          //setNotStakedGrims(notStaked);
-          //setHiddenGrims(hidden);
-          //setPointsLocked(formatPoints(locked));
-          //setPointsUnclaimed(formatPoints(unclaimed));
+          setNotStakedGrims(notStaked);
+          setHiddenGrims(hidden);
+          setPointsLocked(parseFloat(Utils.formatPoints(`${locked}`)));
+          setPointsUnclaimed(parseFloat(Utils.formatPoints(`${unclaimed}`)));
         }
-
+        setIsGrimsLoaded(true)
       } catch (error) {
         console.error('error...', error)
       }
@@ -168,8 +218,70 @@ function GrimsProvider({ children }:any) {
     // loadGrimsRefresh = setInterval(loadGrims, 5 * 60 * 1000);
   }
 
+  const loadPublicState = async () => {
+    isPublicStateLoading = true
+    clearInterval(loadPublicStateRefresh);
+
+    try {
+      let result = await axios.get(getPublicStateURL);
+      
+      if (result && result.data && result.data.success) {
+        setAllStakedGrims(result.data.allStakedTokens);
+
+        SOL_PRICE = parseFloat(result.data.solPrice.USD);
+        FLOOR_PRICE = parseFloat(result.data.floorPrice.SOL);
+
+        loadTVL(result.data.allStakedTokens);
+      }
+
+      loadPublicStateRefresh = setInterval(() => loadPublicState(), 15 * 60 * 1000);
+      setIsPublicStateLoaded(true);
+    } catch (err) {
+      console.error(err)
+    }
+    isPublicStateLoading = false
+  }
+
+  const loadTVL = async (allStakedGrims:number) => {
+    let tlv = FLOOR_PRICE * allStakedGrims;
+
+    setLockedValueSOL(parseFloat(tlv.toFixed(2)));
+    setLockedValueUSD(parseFloat((tlv * SOL_PRICE).toFixed(2)));
+  };
+
   return (
-    <GrimsContext.Provider value={{grims, setGrims, daemons, setDaemons, grimsReady, setGrimsReady, loadGrims, isLoadingGrims, isUsingLedger, setIsUsingLedger, canCreateRaffle, canCreateProposal, grimCount}}>
+    <GrimsContext.Provider value={{
+      grims, 
+      setGrims, 
+      daemons, 
+      setDaemons, 
+      grimsReady, 
+      setGrimsReady, 
+      loadGrims, 
+      isLoadingGrims, 
+      isUsingLedger, 
+      setIsUsingLedger, 
+      canCreateRaffle, 
+      canCreateProposal, 
+      grimCount, 
+      pointsBalance, 
+      setPointsBalance,
+      isGrimsLoaded, 
+      airdropApproved, 
+      hasOnlyUnstakedGrimsInWallet,
+      isClockingIn,
+      isClockingOut,
+      notStakedGrims,
+      hiddenGrims,
+      pointsLocked,
+      setPointsLocked,
+      pointsUnclaimed,
+      setPointsUnclaimed,
+      isPublicStateLoaded,
+      allStakedGrims,
+      lockedValueSOL,
+      lockedValueUSD,
+    }}>
       {children}
     </GrimsContext.Provider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
-import React, { useEffect, useState, useImperativeHandle, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useWallet, useConnection } from '@solana/wallet-adapter-react';
-import { PublicKey, Transaction, TransactionInstruction } from "@solana/web3.js";
 import bs58 from "bs58";
 import Grid from '@mui/material/Grid';
 import Tooltip from '@mui/material/Tooltip';
-import {Buffer} from 'buffer';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
@@ -13,10 +11,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Fade from "@mui/material/Fade";
 import Divider from '@mui/material/Divider';
-import FormSwitch from '@mui/material/Switch';
-import FormControlLabel from "@mui/material/FormControlLabel";
 import { styled } from "@mui/system";
-import ModalUnstyled from "@mui/base/ModalUnstyled";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios";
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
@@ -50,77 +45,56 @@ const tabTheme = createTheme({
 });
 
 const domainURL = process.env.NEXT_PUBLIC_API_URL || '';
-const solanaRPC = process.env.NEXT_PUBLIC_SOLANA_RPC_HOST || '';
 
-const COOLDOWN_UNITS = 10; // used to show a more precise number when displaying the cooldown
-const POINTS_PER_TIMEPERIOD = 10; // points per day
 const PERIOD_PER_TIMEPRIOD = 1; // 1 day per day
 const LOCKING_PERIOD = 14; // 14 days
 const ONE_LOCKING_PERIOD = (24 * 60 * 60 * 1000); // day in milliseconds
 
-const userURL = domainURL + "/user";
 const stakingURL = domainURL + "/stake";
 const unstakingURL = domainURL + "/unstake";
-const getStakedURL = domainURL + "/grims-state";
 const claimPointsURL = domainURL + "/claim-points";
 
-const millisecondsInPeriod = (24 * 60 * 60 * 1000);
 const penaltyCooldown = 7 * 24 * 60 * 60 * 1000; // 7 days
 
-let ONE_COOLDOWN_PERIOD = (24 * 60 * 60 * 1000);
-let WALLET_COOLDOWN_RATE = 1;
-
-let SOL_PRICE = 0;
-let FLOOR_PRICE = 0;
 let requestStarted = false;
-let loadGrimsRefresh = null;
 
 let astraRecalcInterval: NodeJS.Timeout | undefined = undefined;
 
 const Home: NextPage = () => {
-  const { setGrims, isUsingLedger, daemons, setDaemons } = useContext(GrimsContext);
+  const { 
+    loadGrims, 
+    isUsingLedger, 
+    daemons, 
+    pointsBalance, 
+    setPointsBalance, 
+    isLoadingGrims, 
+    isGrimsLoaded, 
+    airdropApproved, 
+    hasOnlyUnstakedGrimsInWallet,
+    isClockingIn, 
+    isClockingOut,
+    notStakedGrims,
+    hiddenGrims,
+    pointsLocked,
+    setPointsLocked,
+    pointsUnclaimed,
+    setPointsUnclaimed,
+} = useContext(GrimsContext);
   const { connection } = useConnection();
-  const { publicKey, wallet, sendTransaction, signTransaction, signMessage } = useWallet();
-  const [isLoadingGrims, setIsLoadingGrims] = useState(false);
+  const { publicKey, wallet, signTransaction, signMessage } = useWallet();
   const [loadingOverlay, setLoadingOverlay] = useState(false);
   const [isClaimingAstra, setIsClaimingAstra] = useState(false);
   const [isClockingOutAll, setIsClockingOutAll] = useState(false);
-  const [isClockingOut, setIsClockingOut] = useState<{[key:string]: boolean}>({});
   const [isClockingInAll, setIsClockingInAll] = useState(false);
-  const [isClockingIn, setIsClockingIn] = useState<{[key:string]: boolean}>({});
   const [stakedGrims, setStakedGrims] = useState<any[]>([]);
-  const [notStakedGrims, setNotStakedGrims] = useState<any[]>([]);
-  const [hiddenGrims, setHiddenGrims] = useState<any[]>([]);
-  const [pointsBalance, setPointsBalance] = useState(0);
-  const [pointsLocked, setPointsLocked] = useState(0);
-  const [pointsUnclaimed, setPointsUnclaimed] = useState(0);
-  const [airdropApproved, setAirdropApproved] = useState(false);
   const [points, setPoints] = useState<any[]>([]);
   const [pointsPerDay, setPointsPerDay] = useState(0);
-  const [isGrimsLoaded, setIsGrimsLoaded] = React.useState(false);
-
-  let clockingOutState = {};
 
   function getTimestamp() {
     let now = new Date();
 
     return Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds());
   }
-
-  const [departmentsBreakdown, setDepartmentsBreakdown] = useState({
-    "BASE_COMMON": 0,
-    "BASE_RARE": 0,
-    "BASE_LEGENDARY": 0,
-    "BASE_MYTHIC": 0,
-    "BASE_ANCIENT": 0,
-    "BASE_GOLDEN_ANCIENT": 0,
-    "BASE_DAEMON": 0
-  });
-
-  const formatPoints = (points:string) => {
-    let p = parseFloat(points);
-    return p >= 100 ? p.toFixed(2) : p.toFixed(3);
-  };
 
   const calculatePoints = (grim:any) => {
     const lockingPeriod = LOCKING_PERIOD * PERIOD_PER_TIMEPRIOD;
@@ -129,7 +103,6 @@ const Home: NextPage = () => {
 
     let unclaimedPoints = 0;
     let lockedPoints = 0;
-    let currentPeriod = 0;
 
     for (let i in grim.stakedInfo) {
       let stakedInfo = grim.stakedInfo[i];
@@ -172,7 +145,7 @@ const Home: NextPage = () => {
       }
     }
 
-    return [formatPoints(`${unclaimedPoints}`), formatPoints(`${lockedPoints}`)];      
+    return [Utils.formatPoints(`${unclaimedPoints}`), Utils.formatPoints(`${lockedPoints}`)];      
   }
 
   const getStaminaValue = (stamina:number, maxStamina:number) => {
@@ -199,120 +172,8 @@ const Home: NextPage = () => {
 
   const calculateAvailablePoints = (grim:any) => {
     let [unclaimed, locked] = calculatePoints(grim);
-    return formatPoints(`${parseFloat(unclaimed) + parseFloat(locked)}`);
+    return Utils.formatPoints(`${parseFloat(unclaimed) + parseFloat(locked)}`);
   };
-
-  const loadGrims = async () => {
-    // clearInterval(loadGrimsRefresh);
-
-    if (publicKey) {
-      let allGrims:{[key:string]: any} = {};
-      let allDaemons:{[key:string]: any} = {};
-
-      try {
-        let result = await axios.get(getStakedURL + "?wallet=" + publicKey);
-        let staked:any[] = [];
-        let notStaked = [];
-        let hidden = [];
-
-        if (result && result.data && result.data.success && result.data.wallet) {
-          setPointsBalance(parseFloat(formatPoints(result.data.wallet.pointsBalance)));
-          setAirdropApproved(result.data.airdropApproved);
-          setDepartmentsBreakdown(result.data.departments);
-
-          WALLET_COOLDOWN_RATE = parseFloat(result.data.wallet.walletCooldownRate);
-          ONE_COOLDOWN_PERIOD = parseFloat(result.data.wallet.oneCooldownInterval);
-
-          let locked = 0;
-          let unclaimed = 0;
-
-          let stakedResults = result.data.wallet.staked;
-          let clockingOut:{[key:string]: boolean} = {};
-          let clockingIn:{[key:string]: boolean} = {};
-
-          for (let i in stakedResults) {
-            clockingOut[stakedResults[i].mint] = false;
-            clockingIn[stakedResults[i].mint] = false;
-            locked += parseFloat(stakedResults[i].pointsLocked);
-            unclaimed += parseFloat(stakedResults[i].pointsUnclaimed);
-            allGrims[i] = stakedResults[i];
-            staked.push(allGrims[i]);
-          }
-
-          setStakedGrims(staked.sort((a, b) => {
-            if (a.stakedTimestamp < b.stakedTimestamp) {
-              return 1;
-            } else if (a.stakedTimestamp > b.stakedTimestamp) {
-              return -1;
-            }
-
-            return 0;
-          }));
-
-          let unstakedResults = result.data.wallet.unstaked;
-
-          for (let i in unstakedResults) {
-            clockingOut[unstakedResults[i].mint] = false;
-            clockingIn[unstakedResults[i].mint] = false;
-            locked += parseFloat(unstakedResults[i].pointsLocked);
-            unclaimed += parseFloat(unstakedResults[i].pointsUnclaimed);
-            allGrims[i] = unstakedResults[i];
-            notStaked.push(allGrims[i]);
-          }
-
-          handleInitialTabSelection(stakedResults, unstakedResults)
-
-          let benchedResults = result.data.wallet.benched;
-
-          for (let i in benchedResults) {
-            clockingOut[benchedResults[i].mint] = false;
-            clockingIn[benchedResults[i].mint] = false;
-            benchedResults[i].benched = true;
-            allGrims[i] = benchedResults[i];
-            notStaked.push(allGrims[i]);
-          }
-
-          let hiddenResults = result.data.wallet.hidden;
-
-          for (let i in hiddenResults) {
-            hiddenResults[i].hidden = true;
-            locked += parseFloat(hiddenResults[i].pointsLocked);
-            unclaimed += parseFloat(hiddenResults[i].pointsUnclaimed);
-            allGrims[i] = hiddenResults[i];
-            hidden.push(allGrims[i]);
-          }
-
-          let daemonsResults = result.data.wallet.daemons;
-
-          for (let i in daemonsResults) {
-            allDaemons[i] = daemonsResults[i];
-          }
-
-          setGrims(allGrims);
-          setDaemons(allDaemons);
-
-          setIsClockingIn(clockingIn);
-          setIsClockingOut(clockingOut);
-
-          setNotStakedGrims(notStaked);
-          setHiddenGrims(hidden);
-          setPointsLocked(parseFloat(formatPoints(`${locked}`)));
-          setPointsUnclaimed(parseFloat(formatPoints(`${unclaimed}`)));
-        }
-
-        setIsGrimsLoaded(true)
-      } catch (error) {
-        console.error('error...', error)
-      }
-    }
-  }
-
-  const handleInitialTabSelection = (stakedResults:any, unstakedResults:any) => {
-    const hasStakedGrims = stakedResults && Object.keys(stakedResults).length > 0
-    const hasUnstakedGrims = unstakedResults && Object.keys(unstakedResults).length > 0
-    const hasOnlyUnstakedGrimsInWallet = !hasStakedGrims && hasUnstakedGrims
-    if (hasOnlyUnstakedGrimsInWallet) setTabValue('onVacation')
-  }
 
   const updateAstraCalc = () => {
     let ps = [];
@@ -340,8 +201,8 @@ const Home: NextPage = () => {
     }
 
     setPoints(ps);
-    setPointsUnclaimed(parseFloat(formatPoints(`${allUnclaimedPoints}`)));
-    setPointsLocked(parseFloat(formatPoints(`${allLockedPoints}`)));
+    setPointsUnclaimed(parseFloat(Utils.formatPoints(`${allUnclaimedPoints}`)));
+    setPointsLocked(parseFloat(Utils.formatPoints(`${allLockedPoints}`)));
   }
 
   const startAstraCalc = () => {
@@ -356,13 +217,10 @@ const Home: NextPage = () => {
   startAstraCalc();
 
   useEffect(() => {
-      async function data() {
-        setIsLoadingGrims(true);
-        await loadGrims();
-        setIsLoadingGrims(false);
-      } 
-      data();
-    }, [publicKey]);
+    if(isGrimsLoaded){
+      if (hasOnlyUnstakedGrimsInWallet) setTabValue('onVacation')
+    }
+  }, [isGrimsLoaded]);
 
   useEffect(() => {
       async function data() {
@@ -565,13 +423,6 @@ const Home: NextPage = () => {
       requestStarted = false
     }
   };
-
-  /*
-  useImperativeHandle(ref, () => ({
-    triggerClaimFunction: () => {
-      triggerClaim();
-    }
-  }));*/
 
   const [tabValue, setTabValue] = useState('clockedIn');
   const [sectionValue, setSectionValue] = useState('staking');
@@ -849,7 +700,7 @@ const Home: NextPage = () => {
                 <Grid columns={24} container spacing={2}>
                 {notStakedGrims &&
                   notStakedGrims.length > 0 &&
-                  notStakedGrims.map((val, ind) => {
+                  notStakedGrims.map((val:any) => {
                     return (
                       <Grid item xs={24} sm={12} md={8} lg={6} key={val.mint} className="grim-card-container m-b-md">
                         <div className="grim-card">
